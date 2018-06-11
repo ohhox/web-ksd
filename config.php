fcee29fbd08e19240457241adcea7e7a30cbdb1d
Wit <wannama.pawit@gmail.com>
2018-06-11 22:07:26 +0700

    config --

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 $server = $_SERVER['SERVER_NAME'];
 define('_DRIVER', 'sqlsrv');
-define('_DBHOST', 'localhost\SQLEXPRESS ');
-define('_DBUSERNAME', '');
-define('_DBPASSWORD', '');
+define('_DBHOST', 'SERVER\SQLEXPRESS');
+define('_DBUSERNAME', 'mrp');
+define('_DBPASSWORD', 'mrp1234');
 define('_DBNAME', 'KSD_MRP');
 define('URL', "http://$server/web-ksd/");
 
